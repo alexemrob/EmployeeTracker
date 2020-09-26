14d8128dd5b4aea2f24129ce12dfaec607a46755
Alexandria Robertson <alexemrob@gmail.com>
2020-09-26 14:15:16 -0500

    start function

--- tracker.js
+++ tracker.js
@@ -3,55 +3,72 @@ var inquirer = require("inquirer");
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
-    host: "localhost",
-  
-    // Your port; if not 3306
-    port: 3306,
-  
-    // Your username
-    user: "root",
-  
-    // Your password
-    password: "",
-    database: "tracker_db"
-  });
-  
-  // connect to the mysql server and sql database
-connection.connect(function(err) {
-    if (err) throw err;
-    // run the start function after the connection is made to prompt the user
-    start();
-  });
+  host: "localhost",
 
-  
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "",
+  database: "tracker_db"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
+
+
 // function which prompts the user for what action they should take
 function start() {
-    inquirer
-      .prompt({
-        name: "toDo",
-        type: "list",
-        message: "Would you like to do?",
-        choices: [
-            "Add department.", 
-            "Add role.", 
-            "Add employee.", 
-            "View department.", 
-            "View role.", 
-            "View employee.", 
-            "Update department.", 
-            "Update role.", 
-            "Update employee."
-        ]
-      })
-      .then(function(answer) {
+  inquirer
+    .prompt({
+      name: "toDo",
+      type: "list",
+      message: "Would you like to do?",
+      choices: [
+        "Add Employee",
+        "Add Department",
+        "Add Role",
+        "View Employees",
+        "View Departments",
+        "View Roles",
+        "Update Employee Role",
+        "End"]
+    })
+    .then(function ({ task }) {
+      switch (task) {
         // based on their answer, either call the propper functions
-        if (answer.toDo === "") {
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Add Department":
           addDepartment();
-        }
-        else if(answer.toDo === "") {
+          break;
+        case "Add Role":
           addRole();
-        } else{
+          break;
+        case "View Employees":
+          viewEmployees();
+          break;
+        case "View Departments":
+          viewDepartments();
+          break;
+        case "View Roles":
+          viewRoles();
+          break;
+        case "Update Employee Role":
+          updateRole();
+          break;
+        case "End":
           connection.end();
-        }
-      });
-  }
+          break;
+
+      }
+    });
+}
